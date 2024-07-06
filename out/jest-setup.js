1614f03a8c23e9ab237c01879374951e4502b661
mcsaur <mcsaur@yahoo.com>
2024-07-06 18:46:00 -0400

    fix: Trying to build the project

--- out/jest-setup.js
+++
@@ -1,6 +0,0 @@
-import 'cli-testing-library/extend-expect';
-import { configure } from 'cli-testing-library';
-/**
- * Adjusted the wait time for waitFor/findByText to 2000ms, because the default 1000ms makes the test results flaky
- */
-configure({ asyncUtilTimeout: 2000 });
